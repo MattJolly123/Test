e70e0d9fbd2b632f70336a0130d8f794650825e7
MattJ <37968615+MattJolly123@users.noreply.github.com>
2021-05-28 00:57:23 +0100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_ids" {
-  value       = azurerm_linux_virtual_machine.linux_vm.*.id
-  description = "Ids of VMs for use in Backup Configs etc"
-}
-
-output "vm_nic_ids" {
-  value       = azurerm_network_interface.linux_vm_nics.*.id
-  description = "Ids of VM Nics for use when using the module"
-}
